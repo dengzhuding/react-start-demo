9954f88efa10d3515638860308543b79bbe0a9c5
dzd_global <dengzhuding@123.com>
2021-02-25 01:18:34 +0800

    搭建基础环境，下一步：游戏完善

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+class Square extends React.Component {
+  constructor (props) {
+    super(props);
+    // 初始化状态值
+    this.state = {
+      value: null
+    }
+    // 绑定this为当前实例
+    this.clickHandle = this.clickHandle.bind(this)
+  }
+  clickHandle (event) {
+    this.setState({value: 'X'})
+  }
+  render() {
+    return (
+      <button className="square" onClick={this.clickHandle}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+  render() {
+    let status = 'Next player : X';
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="bord-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="bord-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="bord-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  // constructor() {
+  // }
+  render() {
+    return (
+      <div className="game">
+          <div className="game-bord">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div></div>
+            <ol></ol>
+          </div>
+      </div>
+    );
+  }
+}
+
+ReactDom.render(
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
